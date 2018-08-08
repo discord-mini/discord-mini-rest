862134e159d9620cc246f41f9f399e7a0938d58f
rmu <r2hkri@gmail.com>
2018-08-07 21:39:25 -0500

    didnt realize how to get headers properly

--- limiter.js
+++ limiter.js
@@ -56,28 +56,28 @@ class LimitHandler {
                 .then(async (res) => {
                     if (res.ok || res.status === 429) {
                         // set limit as most recent one
-                        this.limits[major].limit = res.headers['x-ratelimit-limit'] || this.limits[major].limit;
-                        if (this.limits[major].reset === 0) {
+                        this.limits[major].limit = res.headers.get('x-ratelimit-limit') || this.limits[major].limit;
+                        if (this.limits[major].reset === 0 && res.headers.get('x-ratelimit-reset')) {
                             // if we never got a reset time, set it
-                            this.limits[major].reset = res.headers['x-ratelimit-reset'];
-                        } else if (res.headers['x-ratelimit-reset'] > this.limits[major].reset) {
+                            this.limits[major].reset = res.headers.get('x-ratelimit-reset');
+                        } else if (res.headers.get('x-ratelimit-reset') > this.limits[major].reset) {
                             // if the reset time comes after our known reset, then we must have been reset
-                            this.limits[major].reset = res.headers['x-ratelimit-reset'];
-                            this.limits[major].remaining = res.headers['x-ratelimit-remaining'];
+                            this.limits[major].reset = res.headers.get('x-ratelimit-reset');
+                            this.limits[major].remaining = res.headers.get('x-ratelimit-remaining');
                         } else {
                             // or else our remaining is the smallest of our remaining or what the header says
                             // because we kind of send out multiple requests at the same time
-                            this.limits[major].remaining = Math.min(res.headers['x-ratelimit-remaining'] || this.limits[major].limit, this.limits[major].remaining);
-						}
+                            this.limits[major].remaining = Math.min(res.headers.get('x-ratelimit-remaining') || this.limits[major].limit, this.limits[major].remaining);
+                        }
                         if (res.ok) {
-                            // only way to resolve is with an ok status
-                            resolve([key, await res.json()]);
+							// only way to resolve is with an ok status
+                            resolve([key, res.status === 204 ? '' : await res.json()]);
                         } else {
                             // retry if not res.ok because rate limited
-                            this.limits[major].queue.unshift(func)
-                        }
+							this.limits[major].queue.unshift(func)
+						}
                     } else {
-						// some non rate limit, non ok error
+                        // some non rate limit, non ok error
                         reject([key, await res.json()]);
                     }
                     
